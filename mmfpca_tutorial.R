1083bbf3f994cb86201089e2256bdad7a6dc27c6
md0923 <59901536+md0923@users.noreply.github.com>
2024-12-02 04:34:42 -0800

    Update and rename mmfpca_tutorial to mmfpca_tutorial.R

---
+++ mmfpca_tutorial.R
@@ -0,0 +1,16 @@
+
+
+
+
+###########################################################################################
+# Load packages and required files
+###########################################################################################
+require(Matrix)
+require(MASS)
+require(splines)
+require(mgcv)
+require(mvtnorm)
+require(refund)
+source("eigenfunction_construction.R")
+source("simulation_data_generation.R")
+source("mmfpca.R")
